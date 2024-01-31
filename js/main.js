4bef208ad0b69b40546c6fd9d232864aab0a6d6a
lucapolzone <lucapolzone@gmail.com>
2024-01-31 18:19:07 +0100

    feat: add completed exercise + bonus 1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,43 @@
-  const { createApp } = Vue
+const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        slides: [
-          "./img/01.webp",
-          "./img/02.webp",
-          "./img/03.webp",
-          "./img/04.webp",
-          "./img/05.webp"
-        ]
+createApp({
+  data() {
+    return {
+      activeSlide: 0,
+      slides: [
+        "./img/01.webp",
+        "./img/02.webp",
+        "./img/03.webp",
+        "./img/04.webp",
+        "./img/05.webp"
+      ]
+    }
+  },
+
+  methods: {
+    prevClick() {
+      if (this.activeSlide <= 0) {
+        this.activeSlide = this.slides.length - 1;
+      } else {
+        this.activeSlide--;
       }
     },
-    mounted() {
-      console.log(this.slides);
-    }   
-  }).mount('#app')
+
+    nextClick() {
+
+      if (this.activeSlide >= this.slides.length - 1) {
+        this.activeSlide = 0;
+      }
+      this.activeSlide++;
+    },
+
+
+    gotoSlide(index){
+      this.activeSlide = index;
+    }
+
+
+  }
+
+
+}).mount('#app')
